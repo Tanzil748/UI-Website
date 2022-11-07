d7a944bd5a3fe242dd7880138d992ad572f15315
Tanzil Hassan <tanzilhassan333@gmail.com>
2022-11-07 15:57:51 -0500

    CardDeal done

--- UI-Website/src/components/Button.jsx
+++ UI-Website/src/components/Button.jsx
@@ -4,7 +4,7 @@ export const Button = ({ styles }) => {
   return (
     <button
       type="button"
-      className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] text-orimary outline-none ${styles}`}
+      className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] text-orimary outline-none ${styles} rounded-[10px]`}
     >
       Get Started
     </button>
